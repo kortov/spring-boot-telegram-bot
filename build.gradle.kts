4989348792df7c4a7fcac880fcddfe9d8cf991d8
Eugene Kortov <kortovea@gmail.com>
2019-11-07 23:55:13 +0400

    fix circleci

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,7 @@ tasks.jacocoTestReport {
     reports {
         xml.isEnabled = false
         csv.isEnabled = false
+        html.isEnabled = true
         html.destination = file("${buildDir}/jacocoHtml")
     }
 }
