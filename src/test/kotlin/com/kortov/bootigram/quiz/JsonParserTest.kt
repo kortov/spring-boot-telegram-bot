f75a953e7d1c9f1b6350a19aa0e71002939397da
Eugene Kortov <kortovea@gmail.com>
2019-11-28 18:18:57 +0400

    replace Arrays.asList to listOf

--- src/test/kotlin/com/kortov/bootigram/quiz/JsonParserTest.kt
+++ src/test/kotlin/com/kortov/bootigram/quiz/JsonParserTest.kt
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import java.util.*
 
 
 @ExtendWith(SpringExtension::class)
@@ -27,12 +26,11 @@ class JsonParserTest {
         val fileName = "static/quiz_simplest_sample.json"
         val string = DefaultResourceLoader().getResource(fileName).file.readText(Charsets.UTF_8)
         val result = jsonParser.parse(string)
-        val expected = Arrays.asList(Exam(
-                "name", Arrays.asList(
-                Chapter(0, "Chapter 1", Arrays.asList(
-                        ExamQuestion(0, "questionText", "explanation",
-                                Arrays.asList(Answer(1, "textAnswer")),
-                                Arrays.asList(1)))))))
+        val expected = listOf(Exam("name",
+                listOf(Chapter(0, "Chapter 1",
+                        listOf(ExamQuestion(0, "questionText", "explanation",
+                                listOf(Answer(1, "textAnswer")),
+                                listOf(1)))))))
         Assertions.assertEquals(
                 expected, result)
     }
